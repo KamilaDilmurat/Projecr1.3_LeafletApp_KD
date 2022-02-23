6fd81a33fdcf8f9f45018fd3376cc31e28857a76
KamilaDilmurat <89537254+KamilaDilmurat@users.noreply.github.com>
2022-02-22 22:58:14 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-var ConcentrationCamps = L.map('ConcentrationCamps').setView([43.8,87.6], 10);
-
+var map = L.map('ConcentrationCamps').setView([43.8,87.6], 6);
 
 
 // load a tile layer
@@ -7,7 +6,7 @@ var ConcentrationCamps = L.map('ConcentrationCamps').setView([43.8,87.6], 10);
 	attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 	subdomains: 'abcd',
 	minZoom: 10,
-	maxZoom: 500,
+	maxZoom: 20,
 	ext: 'png'
 }).addTo(ConcentrationCamps);
 
