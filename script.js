801aece07d21b78c8b895e0fbd8e6e61f573bd81
KamilaDilmurat <89537254+KamilaDilmurat@users.noreply.github.com>
2022-02-23 16:52:09 -0600

    Update script.js

--- script.js
+++ script.js
@@ -3,13 +3,19 @@
 var map = L.map('mapid').setView([43.88,81.31], 11);
 
   // load a tile layer
- L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}/{x}/{y}{r}.{ext}', {
-	attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-	subdomains: 'abcd',
-	minZoom: 0,
+ //L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}/{x}/{y}{r}.{ext}', {
+	//attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+	//subdomains: 'abcd',
+	//minZoom: 0,
+	//maxZoom: 20,
+	//ext: 'png'
+//}).addTo(map);
+
+L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png', {
 	maxZoom: 20,
-	ext: 'png'
+	attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
 }).addTo(map);
+
 
 var geojsonFeature = {
   "type": "FeatureCollection",
