2fe3bf611483103ae2b2ae58008ca9b4515218cd
KamilaDilmurat <89537254+KamilaDilmurat@users.noreply.github.com>
2022-02-22 23:57:10 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,142 @@
-var map = L.map('ConcentrationCamps').setView([43.8,87.6], 6);
 
 
-// load a tile layer
+var map = L.map('mapid').setView([43.88,81.31], 11);
+
+  // load a tile layer
  L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}/{x}/{y}{r}.{ext}', {
 	attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 	subdomains: 'abcd',
-	minZoom: 10,
+	minZoom: 0,
 	maxZoom: 20,
 	ext: 'png'
-}).addTo(ConcentrationCamps);
+}).addTo(map);
 
+var geojsonFeature = {
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {},
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          81.1409854888916,
+          43.977653207646604
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {},
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          81.31009340286255,
+          43.96390897601157
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {},
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          81.30632758140564,
+          43.89873505867078
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {},
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          81.3196849822998,
+          43.909047104752574
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {},
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          81.3897442817688,
+          43.90276268955311
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {},
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          81.47894382476807,
+          43.95689664852915
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {},
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          81.50055170059204,
+          43.975800238332745
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {},
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          81.49572372436523,
+          43.974997267006266
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {},
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          81.49619579315186,
+          43.97851790687153
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {},
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          81.53165459632874,
+          43.978919370088335
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {},
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          81.53374671936035,
+          44.000131294892014
+        ]
+      }
+    }
+  ]
+};
+var feat = L.geoJSON(geojsonFeature).addTo(map);
 
-var marker1 = L.marker([39.753, -105.053]).addTo(ConcentrationCamps);
-var marker2 = L.marker([39.741, -104.977]).addTo(ConcentrationCamps);
-var marker3 = L.marker([39.762, -105.030]).addTo(ConcentrationCamps);
-var marker4 = L.marker([39.743, -104.990]).addTo(ConcentrationCamps);
-var marker5 = L.marker([39.745, -104.992]).addTo(ConcentrationCamps);
-
-
-marker1.bindPopup("<b>Resturant 1</b><br>US Thai Cafe").openPopup();
-marker2.bindPopup("<b>Resturant 2</b><br>Phở-natic").openPopup();
-marker3.bindPopup("<b>Resturant 3</b><br>TACOS TEQUILA WHISKEY").openPopup();
-marker4.bindPopup("<b>Resturant 4</b><br>Yarh House").openPopup();
-marker5.bindPopup("<b>Resturant 5</b><br>Mad Greens ").openPopup();
+feat.bindPopup("<b>Hello world! this is a camp </b><br>this is where innocent uyghurs locked up .").openPopup();
